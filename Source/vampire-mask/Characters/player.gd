0d956326ae5eb52ab2a27783de25f12628ce4e8f
Justas T <justas.tomkus@sneakybox-studios.com>
2026-01-31 02:01:33 +0200

    add initial placeholder character with movement logic

---
+++ Source/vampire-mask/Characters/player.gd
@@ -0,0 +1,47 @@
+extends CharacterBody2D
+
+@export var anim: NodePath
+@onready var anim_: AnimatedSprite2D = get_node(anim) as AnimatedSprite2D
+
+@export var max_speed: float = 100.0
+@export var acceleration: float = 300.0
+@export var friction: float = 400.0
+@export var dash_imp: float = 1000.0
+@export var dash_fric: float = 0.6
+@export var dash_cd: float = 1.0
+
+
+var motion = Vector2.ZERO
+
+func _physics_process(delta: float) -> void:
+	
+	var move_vecn :Vector2 = Input.get_vector("MoveLeft", "MoveRight", "MoveDown", "MoveUp").normalized()
+		
+	apply_movement_friction(move_vecn, delta)
+	move_and_slide()
+	motion = get_real_velocity()
+	apply_animation()
+
+func apply_movement_friction(in_vec: Vector2, delta: float) -> void:
+	if in_vec != Vector2.ZERO:
+		motion = motion.move_toward(in_vec * max_speed, acceleration * delta)
+	else:
+		motion = motion.move_toward(Vector2.ZERO, friction * delta)
+	velocity = motion
+	
+
+func apply_animation() -> void:
+	if motion == Vector2.ZERO:
+		# if walking sfx -> stop
+		if anim_.is_playing():
+			anim_.stop()
+			anim_.frame = 0
+	else:
+		pass
+		if motion.x < 0:
+			anim_.flip_h = true
+		elif motion.x > 0:
+			anim_.flip_h = false
+		# if !walk sfx -> play
+		if !anim_.is_playing():
+			anim_.play()
