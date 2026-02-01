d1d085ccf51e8a283b2efc2410c309c9adebe8cd
Miglė <debesupastas@gmail.com>
2026-02-01 11:15:43 +0200

    Polished level pictures

---
+++ Source/vampire-mask/General/DataTypes_alt.gd
@@ -0,0 +1,17 @@
+extends Node
+
+
+enum MaskTypes {
+	Undefined = 0,
+	Sun = 1,
+	Human = 2,
+	Garlic = 3,
+	None = 4,
+}
+
+enum InterTypes {
+	Undefined = 0,
+	AddMask = 1,
+	OpenDoor = 2,
+	ForceMask = 3,
+}
